ed8749dd2e863a6dd138fe1243101575352f8fb7
Emanuel Gomes <dapouco3@gmail.com>
2026-02-09 16:19:25 -0300

    build:Update drift dependencies

--- lib/app/features/tab/data/datasources/remote/tab_remote_data_source_impl.dart
+++
@@ -1,7 +0,0 @@
-// app/features/tab/data/datasources/remote/tab_remote_data_source_impl.dart
-import 'tab_remote_data_source.dart';
-
-
-class TabRemoteDataSourceImpl implements TabRemoteDataSource {
-    
-}
--- lib/app/features/tab/domain/repo/tab_repo.dart
+++ lib/app/features/tab/domain/repo/tab_repo.dart
@@ -3,6 +3,7 @@
 import 'package:tab_writer/app/features/tab/domain/entities/tab_entity.dart';
 
 abstract class TabRepository {
+
   Future<List<TabEntity>> getAllTabs();
 
   Future<TabEntity> getTabById(int id);
@@ -10,5 +11,7 @@ abstract class TabRepository {
   Future<void> saveTab(TabEntity tab);
 
   Future<void> deleteTab(int id);
+
   Future<void> updateTab(TabEntity tab);
+  
 }
